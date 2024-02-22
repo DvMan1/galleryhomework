866e66362bf3e14b146de12d0b41c2670ea9e034
Egor <msubzero2001@gmail.com>
2024-02-22 21:37:42 +0200

    searchPhoth

--- src/public/pixabay-api.js
+++ src/public/pixabay-api.js
@@ -1,6 +1,13 @@
 const API_KEY = '42511609-dd42bb6c326d646bc46fbc16c';
 export function searchPhoth(keyword) {
- return fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${keyword}`).then(resp => {
+    const urlPar = new URLSearchParams({
+        key:API_KEY,
+        q:keyword,
+        image_type:"photo",
+        orientation:"horizontal",
+        safesearch:"true",
+    })
+ return fetch(`https://pixabay.com/api/?key=${API_KEY}&${urlPar}`).then(resp => {
     if (!resp.ok) {
       throw new Error(resp.statusText);
     }
