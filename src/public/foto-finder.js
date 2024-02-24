2cb3dbae903999a2c0daac65deab7c5de2a4a450
Egor <msubzero2001@gmail.com>
2024-02-24 15:20:59 +0200

    Interval

--- src/public/foto-finder.js
+++ src/public/foto-finder.js
@@ -1,37 +1,54 @@
-
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import {searchPhoth} from "./pixabay-api";
-import { showPhoto } from "./render-finc";
-
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { searchPhoth } from './pixabay-api';
+import { showPhoto } from './render-finc';
 
 const refInput = document.querySelector('.search-field');
 const refButton = document.querySelector('.button-search');
-const refConteiner = document.querySelector('.photo-container')
-
+const refConteiner = document.querySelector('.gallery');
 refButton.addEventListener('click', searchFoto);
 
 function searchFoto() {
-    const keyword = refInput.value;
-    searchPhoth(keyword)
+  const keyword = refInput.value;
+  searchPhoth(keyword)
     .then(data => {
-        if (!Array.isArray(data.hits) || data.hits.length === 0) {
-          throw new Error('No photos found');
+      if (!Array.isArray(data.hits) || data.hits.length === 0) {
+        throw new Error('No photos found');
+      }
+      return data;
+    })
+    .then(data => {
+      const arrElImg = showPhoto(data.hits);
+      refConteiner.innerHTML = arrElImg;
+     const intervalId = setInterval(() => {
+        const ArrElImg = document.querySelectorAll('.loader');
+        if (ArrElImg.length === 0) {
+          clearInterval(intervalId);
+        } else {
+          ArrElImg.forEach(img => {
+            if (img.complete && img.classList.contains('loader')) {
+              img.classList.remove('loader');
+            }
+          });
         }
-        return data;})
-      .then((data) => {  
-        const arrElImg = showPhoto(data.hits);
-        refConteiner.innerHTML = arrElImg
-        refInput.value =""
-        
-      })
-      .catch((err) => {
-        iziToast.show({
-          title: 'Ошибка',
-          message: `${err}`,
-          backgroundColor: "red"
-        });
+      }, 50);
+      const lightbox = new SimpleLightbox('.gallery a', {
+        caption: true,
+        captionsSelector: 'img',
+        captionsType: 'attr',
+        captionsData: 'alt',
+        captionDelay: 250,
       });
-  }
-  
- 
+      refInput.value = '';
+    })
+    .catch(err => {
+      iziToast.show({
+        title: 'Ошибка',
+        message: `${err}`,
+        backgroundColor: 'red',
+      });
+      refInput.value = '';
+    });
+}
--- src/public/render-finc.js
+++ src/public/render-finc.js
@@ -1,6 +1,19 @@
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 export function showPhoto(arrPhoto) {
+  console.log(arrPhoto);
+
     const arrElImg = arrPhoto.map(photo => {
-      return `<img src="${photo.previewURL}" alt="Фото">`;
-    }).join("");
+      return `<li class="gallery-item list">
+     <a class="gallery-link" href="${photo.largeImageURL}">
+        <img class="gallery-image loader" src="${photo.previewURL}" alt="${photo.tags}" />
+      </a>
+    </li>`;
+    }).join("")
+ 
+    
     return arrElImg;
   }
+
